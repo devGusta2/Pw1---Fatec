bcaf0c41abbc1f9a47c5e2d6de5acf48630d8e30
devgusta2 <gustavo.souza2979@gmail.com>
2024-10-29 10:39:32 -0300

    Variaveis Var ou Let

---
+++ Aula - 09/js/variables.js
@@ -0,0 +1,34 @@
+// teste de variaveis
+// atribuindo valores
+let idade = 25;
+console.log(idade);
+
+// modificando valores
+idade = 20;
+console.log(idade);
+
+//recuperando valores
+
+idade +=1;
+console.log(idade);
+
+//var ou let ou const
+
+
+function exemploVar(){
+    var x = 5;
+    if(true){
+        var x = 10;
+        console.log(x);
+    }
+    console.log(x);
+}
+// let nao permite a redeclaração em um escopo
+function exemploLet(){
+    let x = 5;
+    if(true){
+        let x = 10;
+        console.log(x);
+    }
+    console.log(x);
+}
