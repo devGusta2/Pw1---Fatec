3baa80b033d2f50313bcccb79778063700ffb135
devgusta2 <gustavo.souza2979@gmail.com>
2024-10-29 11:01:40 -0300

    Tipo de variaveis

--- Aula - 09/js/variables.js
+++ Aula - 09/js/variables.js
@@ -33,6 +33,23 @@ function exemploLet(){
     console.log(x);
 }
 
+// essa função dará erro pois nao é possível reatribuir valores para
+// constantes
+function exemploConsts(){
+    const x = 5;
+    // const y , não existe const vazia, é necessário inicializar
+    if(true){
+        const x = 10;
+        console.log(x);
+    }
+    x = 15;
+    console.log(x);
+}
+function tipoVariaveis(){
+    let idade = 25;
+    let peso  = 80.00;
+}
 // chamando as funções para a execução
 exemploLet();
 exemploVar();
+exemploConsts();
