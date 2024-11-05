ced7532e8a15e7fd988a1eddf6bc531612b6cfc4
= <=>
2024-11-05 12:11:03 -0300

    Aula 10 - DOM

---
+++ Aula - 10 - Seletores/Js/index.js
@@ -0,0 +1,11 @@
+//query selector é usado par apegar elemeto do proprio html ou class
+
+//selectionamos os elemtos no HTML
+
+const titulo = document.getElementById("titulo");
+const text = document.querySelector(".texto");
+const botao = document.getElementById("botao");
+
+botao.addEventListener("click", ()=>{
+    alert('Php is better');
+});
