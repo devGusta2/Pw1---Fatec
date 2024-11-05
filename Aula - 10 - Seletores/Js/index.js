09355e07ab3d5f0056facddc096381c1d3836300
devgusta2 <gustavo.souza2979@gmail.com>
2024-11-05 12:44:46 -0300

    Adicionando elementos via JS

--- Aula - 10 - Seletores/Js/index.js
+++ Aula - 10 - Seletores/Js/index.js
@@ -13,11 +13,18 @@ botao.addEventListener("click", ()=>{
 
 
 //manipulando conteudo e estilos
-titulo.textContent = "Aula com o DOM em JS";
-titulo.innerHTML = "<strong>Texto escuro</strong>";
+// titulo.textContent = "Aula com o DOM em JS";
+// titulo.innerHTML = "<strong>Texto escuro</strong>";
 
-text.style.fontSize = "20px";
+// text.style.fontSize = "20px";
 
 
 //Manipulando elementos
 //criando um elemento
+const novoParagrafro = document.createElement('p')
+
+novoParagrafro.textContent = "Meu novo paragrafo"
+document.body.appendChild(novoParagrafro)
+
+const div = document.getElementsByClassName("textoInicio")
+document.div.append(div);
