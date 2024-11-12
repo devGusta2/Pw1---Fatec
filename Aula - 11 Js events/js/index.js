8d87f8e3195dfb0b583d3a6e52ed56ddd33d5430
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-12 10:09:45 -0300

    Form

--- Aula - 11 Js events/js/index.js
+++ Aula - 11 Js events/js/index.js
@@ -6,3 +6,29 @@ div.addEventListener('mouseover',()=>{
 div.addEventListener('mouseout',()=>{
     div.style.backgroundColor='rgb(129, 207, 181)';
 });
+
+var text = document.getElementById('entrada');
+
+
+
+
+// quando eu digito alguma coisa ele alerta
+text.addEventListener('keyup',(e)=>{
+    alert("Tecla pressionada " + e.key);
+})
+
+let form = document.getElementById('myform');
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let ent_nome = document.getElementById('ent_nome').value;
+    alert("O nome é " + ent_nome);
+    alert("Formulário enviado");
+})
+
+//quando o valor de origem é alterado
+
+// let entrada = document.getElementById('entrada2');
+// entrada.addEventListener('change', (e)=>{
+//     console.log("Valor alterado " + e.target.value)
+// })
+
