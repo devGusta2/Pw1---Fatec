008c0614e58d140957a8bab085ec6f0b7ab7cc6d
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-12 09:46:59 -0300

    Mouse over - Mouse out - JS

---
+++ Aula - 11 Js events/js/index.js
@@ -0,0 +1,8 @@
+var div = document.getElementById('minha_div1');
+
+div.addEventListener('mouseover',()=>{
+    div.style.backgroundColor='blue';
+});
+div.addEventListener('mouseout',()=>{
+    div.style.backgroundColor='rgb(129, 207, 181)';
+});
