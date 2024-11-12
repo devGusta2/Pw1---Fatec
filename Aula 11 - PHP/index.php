85ab7561c7beaa7cbd6105153471bc9db8c4b2e1
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-12 11:00:52 -0300

    Variaveis php

--- Aula 11 - PHP/index.php
+++ Aula 11 - PHP/index.php
@@ -1,5 +1,15 @@
 <?php
 
     echo "Olá";
+    //var_dump - exibe uma array
+    //print_r - imprime arrays e strings
 
+    $veiculo = array("Gol", "Uno", "Celta", "Corsa", "Palio");
+    echo "<br>";
+    // imprime a tipagem
+    echo "<h3>Imprimindo com VAR dump</h3>";
+    var_dump($veiculo);
+    echo "<br>";    
+    //mais simples
+    print_r($veiculo);
 ?>
---
+++ Aula 11 - PHP/variaveis.php
@@ -0,0 +1,25 @@
+<?php
+
+
+    $nome = 'Gusta';
+    $funcao = "Aluno";
+    echo "Nome: $nome";
+    echo "Função: $funcao";
+
+
+    $altura = 1.70;
+    $peso =  80;
+
+
+
+    echo "<h3>Situação</h3>";
+    $situacao = false;
+    echo "<h3>Situação</h3>";
+    $situacao = true;
+    echo "<h3>Situação</h3>";
+    $situacao = Null;
+
+    $disciplina = ['Pw1', 'Bd', 'Estrutura de dados'];
+    print_r($disciplina);
+    echo "<a src='./variaveis.php'>Variaveis</a>";
+?>
