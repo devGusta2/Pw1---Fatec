105c6f6cd86d0abe556db6305447a42d85bb5bd7
devgusta2 <gustavo.souza2979@gmail.com>
2024-11-26 10:48:00 -0300

    Composer

---
+++ Aula 12 - MVC/config/connection.php
@@ -0,0 +1,23 @@
+<?php
+    use PDO;
+    class connection{
+        private $host = "localhost";
+        private $password = '';
+        private $database = 'fatecFv';
+        private $user = 'root';
+
+        public $conn;
+
+        public function getConn(){
+            try{
+
+            }catch(){
+                 
+            }
+        }
+    }
+
+
+
+
+?>
